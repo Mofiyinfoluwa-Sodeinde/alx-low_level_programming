77af73003dace4821512f8c26c9bf60096ead36c
Olusegun Sodeinde <segunsodeinde.com>
2022-10-13 15:25:37 -0700

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - prints
+ * - Dora Korpar, 2015-10-19,
+ * Return: 0 if closed properly, otherwise 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+		return (0)
