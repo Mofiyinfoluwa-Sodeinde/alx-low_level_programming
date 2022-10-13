ddb92fc9caa7a8d6c2f3427913bbbbda9429fd6c
Olusegun Sodeinde <segunsodeinde.com>
2022-10-13 14:35:08 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,7 @@
  * Return: 0 if closed properly, otherwise 1
  */
 int main(void)
-
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
