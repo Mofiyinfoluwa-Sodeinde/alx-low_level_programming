f393b74e88f5a74e2383a9736d2413d8ec7fe87c
Olusegun Sodeinde <segunsodeinde.com>
2022-10-13 14:41:30 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * This c code prints with proper grammar, but the outcome is a piece of art,
- */
+ * main -prints the phrase with proper grammar, 
+ * but the outcome is a piece of art,
+ * Return: 0 if closed properly, otherwise 1
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-		return (0);
+	return (0);
 }
