7403f9ac36a0ff9ffe5adfb0d44326a01a2bb6fb
Olusegun Sodeinde <segunsodeinde.com>
2022-10-13 14:27:26 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: 0 if closed properly, otherwise 1
+ */
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+   puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
 
