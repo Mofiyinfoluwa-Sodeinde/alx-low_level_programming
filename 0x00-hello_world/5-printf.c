6af3dd5d3d8a8158516b73773c90e582f0e73918
Olusegun Sodeinde <segunsodeinde.com>
2022-10-13 14:47:14 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - prints the phrase with proper grammar, 
  * but the outcome is a piece of art,
  * Return: 0 if closed properly, otherwise 1
-*/
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
