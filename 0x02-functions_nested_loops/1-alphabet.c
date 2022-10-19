de859e7157b491236b495d43fb3aa8e63fcb2b34
Olusegun Sodeinde <segunsodeinde.com>
2022-10-19 10:16:26 -0700

    I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char qe;
+
+	for (qe = 'a'; qe <= 'z'; qe++)
+		_putchar(qe);
+
+	_putchar('\n');
+}
