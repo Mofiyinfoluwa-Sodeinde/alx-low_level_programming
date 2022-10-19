b7f53be0d2a6d9fef76367267e8c66f92569f982
Olusegun Sodeinde <segunsodeinde.com>
2022-10-19 10:24:27 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char qe;
+
+	for (ten = 0; ten <= 9; ten++)
+
+		for (qe = 'a'; qe <= 'z'; qe++)
+			_putchar(qe);
+	_putchar('\n')
+}
