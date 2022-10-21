5fa536d3055eb6ef67b552512591c35a8910a179
Olusegun Sodeinde <segunsodeinde.com>
2022-10-21 12:33:51 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,12 +10,14 @@ void print_triangle(int n)
 
 	if (n > 0)
 	{
-		for (h = 1; <= n; h++)
+		for (h = 1; h <= n; h++)
 		{
 			for (tri = n - h; tri > 0; tri--)
 				_putchar(' ');
+
 			for (tri = 0; tri < h; tri++)
 				_putchar('#');
+
 			if (h == n)
 				continue;
 
